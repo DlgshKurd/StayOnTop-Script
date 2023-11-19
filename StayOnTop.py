5224205b51e98e2a83003132d335f40e6e9e6c18
DlgshKurd <96995162+DlgshKurd@users.noreply.github.com>
2023-11-19 15:30:02 +0100

    Update StayOnTop.py
    adding countdown

--- StayOnTop.py
+++ StayOnTop.py
@@ -4,14 +4,13 @@ import win32gui
 import win32con
 
 def make_always_on_top(window):
-
     win32gui.SetWindowPos(window._hWnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
     print(f'Success! {window.title} is now always on top.')
 
 if __name__ == "__main__":
-   
-    time.sleep(3)
-    
+    for i in range(3, 0, -1):
+        print(f"Switch to the window you want to stay on top. Countdown: {i}")
+        time.sleep(1)
+
     focused_window = gw.getActiveWindow()
-    
     make_always_on_top(focused_window)
